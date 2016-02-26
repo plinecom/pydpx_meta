c2a6fcf362e07f0020852b4c95a775b8fa151fea
Masataka <plinecom@gmail.com>
2016-02-26 15:19:50 +0900

    GenericOrientationHeader

--- sample.py
+++ sample.py
@@ -33,7 +33,16 @@ class DpxGenericImageElementBigEndian(ctypes.BigEndianStructure):
         ('LowQuantity',ctypes.c_float),
         ('HighData',ctypes.c_uint32),
         ('HighQuantity',ctypes.c_float),
-        ('Reserved',ctypes.c_char*52)
+        ('Descripter',ctypes.c_byte),
+        ('Transfer',ctypes.c_byte),
+        ('Colorimetric',ctypes.c_byte),
+        ('BitSize',ctypes.c_byte),
+        ('Packing',ctypes.c_uint16),
+        ('Encoding',ctypes.c_uint16),
+        ('DataOffset', ctypes.c_uint32),
+        ('EndOfLinePadding', ctypes.c_uint32),
+        ('EndOfImagePadding', ctypes.c_uint32),
+        ('Description',ctypes.c_char*32)
     ]
 
 
@@ -43,14 +52,33 @@ class DpxGenericImageHeaderBigEndian(ctypes.BigEndianStructure):
         ('NumberElements', ctypes.c_uint16),
         ('PixelsPerLine',ctypes.c_uint32),
         ('LinesPerElement',ctypes.c_uint32),
+        ('ImageElement', DpxGenericImageElementBigEndian*8),
         ('Reserved',ctypes.c_char*52)
     ]
 
 
+class DpxGenericOrientationHeaderBigEndian(ctypes.BigEndianStructure):
+    _fields_ = [
+        ('XOffset', ctypes.c_uint32),
+        ('YOffset', ctypes.c_uint32),
+        ('XCenter', ctypes.c_float),
+        ('YCenter', ctypes.c_float),
+        ('XOriginalSize', ctypes.c_uint32),
+        ('YOriginalSize', ctypes.c_uint32),
+        ('FileName', ctypes.c_char*100),
+        ('TimeData', ctypes.c_char*24),
+        ('InputName', ctypes.c_char*32),
+        ('InputSN', ctypes.c_char*32),
+        ('Border', ctypes.c_uint16*4),
+        ('AspectRatio', ctypes.c_uint32*2),
+        ('Reserved',ctypes.c_byte*28)
+    ]
+
 class DpxBigEndian(ctypes.BigEndianStructure):
     _fields_ = [
-        ('GenericHeader',DpxGenericHeaderBigEndian),
-        ('GenericImageHeader',DpxGenericImageHeaderBigEndian)
+        ('FileHeader',DpxGenericHeaderBigEndian),
+        ('ImageHeader',DpxGenericImageHeaderBigEndian),
+        ('OrientHeader',DpxGenericOrientationHeaderBigEndian)
     ]
 fp = open("/root/V14_37_26_01_v001.0186.dpx","rb")
 fpw = open("/root/test.dpx","wb")
@@ -58,19 +86,22 @@ fpw = open("/root/test.dpx","wb")
 
 dpx = DpxBigEndian()
 fp.readinto(dpx)
-print dpx.GenericHeader.Magic
-print dpx.GenericHeader.ImageOffset
-print dpx.GenericHeader.Version
-print dpx.GenericHeader.FileSize
-print dpx.GenericHeader.DittoKey
-print dpx.GenericHeader.IndustrySize
-print dpx.GenericHeader.UserSize
-print dpx.GenericHeader.FileName
-print dpx.GenericHeader.TimeData
-print dpx.GenericHeader.Creator
-print dpx.GenericHeader.Project
-print dpx.GenericImageHeader.Orientation
-print dpx.GenericImageHeader.NumberElements
+print dpx.FileHeader.Magic
+print dpx.FileHeader.ImageOffset
+print dpx.FileHeader.Version
+print dpx.FileHeader.FileSize
+print dpx.FileHeader.DittoKey
+print dpx.FileHeader.IndustrySize
+print dpx.FileHeader.UserSize
+print dpx.FileHeader.FileName
+print dpx.FileHeader.TimeData
+print dpx.FileHeader.Creator
+print dpx.FileHeader.Project
+print dpx.ImageHeader.Orientation
+print dpx.ImageHeader.NumberElements
+print dpx.ImageHeader.ImageElement[0].Description
+print dpx.OrientHeader.XOriginalSize
+print dpx.OrientHeader.YOriginalSize
 magic = ""
 if magic == "SDPX":
     big_endian = True

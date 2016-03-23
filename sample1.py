e33b49a40eb4ef81a1ac6721d2c752908c42b165
Masataka <plinecom@gmail.com>
2016-03-23 15:35:37 +0900

    サンプル1を整理

--- sample1.py
+++ sample1.py
@@ -1,11 +1,30 @@
 import pydpx_meta
 
+# create instance
 dpx = pydpx_meta.DpxHeader("/root/V14_37_26_01_v001.0186.dpx")
 #dpx = pydpx_meta.DpxHeader()
-print dpx.raw_header.FileHeader.Magic
+
+# normal access
 print dpx.file_header.magic
-print dpx.raw_header.FileHeader.ImageOffset
 print dpx.file_header.image_offset
+print dpx.image_header.image_element[0].data_sign
+print dpx.tv_header.time_code
+print dpx.tv_header.frame_rate
+
+# edit and save sample
+print "- edit and save sample -"
+# change time code
+print dpx.tv_header.time_code
+dpx.tv_header.timecode ="01:23:45:12"
+print dpx.tv_header.time_code
+# change orientation
+dpx.raw_header.ImageHeader.Orientation =2
+# commit change
+dpx.save("/root/test.dpx")
+
+# header table direct access
+print dpx.raw_header.FileHeader.Magic
+print dpx.raw_header.FileHeader.ImageOffset
 print dpx.raw_header.FileHeader.Version
 print dpx.raw_header.FileHeader.FileSize
 print dpx.raw_header.FileHeader.DittoKey
@@ -18,22 +37,10 @@ print dpx.raw_header.FileHeader.Project
 print dpx.raw_header.ImageHeader.Orientation
 print dpx.raw_header.ImageHeader.NumberElements
 print dpx.raw_header.ImageHeader.ImageElement[0].Description
-print dpx.image_header.image_element[0].data_sign
+
 print dpx.raw_header.OrientHeader.XOriginalSize
 print dpx.raw_header.OrientHeader.YOriginalSize
 print dpx.raw_header.TvHeader.TimeCode
-print '{0:0>8x}'.format(dpx.raw_header.TvHeader.TimeCode)
-timecode_tmp = '{0:0>8x}'.format(dpx.raw_header.TvHeader.TimeCode)
-timecode_str = timecode_tmp[0:2]+":"+timecode_tmp[2:4]+":"+timecode_tmp[4:6]+":"+timecode_tmp[6:8]
-print timecode_str
 print dpx.raw_header.TvHeader.UserBits
 print dpx.raw_header.TvHeader.FrameRate
-
-print "test"
-print dpx.tv_header.time_code
-dpx.tv_header.timecode ="01:23:45:12"
-print dpx.tv_header.time_code
-# change orientation
-dpx.raw_header.ImageHeader.Orientation =2
-#dpx.save("/root/test.dpx")
 
--- sample2_ex.py
+++ sample2_ex.py
@@ -1,6 +1,6 @@
-
 import pydpx_meta
 
+# High level class DpxHeaderEx sample
 dpx = pydpx_meta.DpxHeaderEx("/root/V14_37_26_01_v001.0186.dpx")
 print dpx.describe()
 

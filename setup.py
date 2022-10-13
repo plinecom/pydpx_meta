2b78da5eadf7983bb1e75e65625fd70cd917fe36
plinecom <plinecom@gmail.com>
2022-10-13 11:44:06 +0900

    update version

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from setuptools import setup
 
 setup(
     name="pydpx_meta",
-    version="1.1.1",
+    version="2.0.0",
     author="Masataka Koike",
     author_email="plinecom@gmail.com",
     description="Python dpx image meta data read write library",
     license="MIT",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
     keywords="dpx image meta header",
     url="https://github.com/plinecom/pydpx_meta",
     packages=['pydpx_meta'],
